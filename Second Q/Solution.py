8bc67e1a0b2646de41cd68ac29f29f06e8a471ab
Etai zilberman <etai444@gmail.com>
2024-02-27 00:07:50 +0200

    Adding second Q and Sol

--- Second Q/Solution.py
+++ Second Q/Solution.py
@@ -1,1 +1,11 @@
-#Add the second question solution
+
+def removeAnElement(nums: list[int], val: int) -> int:
+        retCounter = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] != val:
+                retCounter += 1
+                i += 1
+            else:
+                nums.pop(i)
+        return retCounter
